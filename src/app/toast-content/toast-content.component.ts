cb01355190d5a6fc3be808b60bd58ef3a2539bdc
maymashd <maymashd@mail.ru>
2020-12-03 20:43:24 +0600

    Toast animation
    developed version

--- src/app/toast-content/toast-content.component.ts
+++ src/app/toast-content/toast-content.component.ts
@@ -12,16 +12,35 @@ import {IToaster} from '../../modules';
 export class ToastContentComponent implements OnInit {
 
   @Input() toast: IToaster;
+  @Input() className?: string;
   @Output() removeToast: EventEmitter<number> = new EventEmitter<number>();
+
+  removeAnimationClass = '';
+  animationRemove = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.removeAnimationClass = `${this.className}_remove`;
     if (this.toast.duration) {
       setTimeout(() => {
-        this.removeToast.emit(this.toast.id);
+        this.animationRemove = true;
+        setTimeout(() => {
+          this.removeToast.emit(this.toast.id);
+          this.animationRemove = false;
+        }, 3000);
       }, this.toast.duration * 1000);
     }
+  }
+
+  onRemoveToast(id: number) {
+    this.animationRemove = true;
+    setTimeout(() => {
+      this.animationRemove = false;
+      setTimeout(() => {
+        this.removeToast.emit(id);
+      }, this.toast.duration * 1000);
+    }, 3000);
   }
 
 }
